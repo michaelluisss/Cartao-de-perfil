c25ded7e82810d6fc9da2a94dd0ba958669ea41c
zzMAGOozz <ldmichael838@gmail.com>
2025-10-27 21:08:37 -0300

    Adicionar o site de lista de tarefas

--- Site-Lista-de-Tarefas/script.js
+++ Site-Lista-de-Tarefas/script.js
@@ -1,24 +1,89 @@
-function tarefas() {
-  // Entradas do Usuario
-  let inserirLista = document.getElementById("inserirLista");
-  let tarefa = inserirLista.value;
+let tarefas = []
+let contador = 0
 
-  // Criando a lista
-  let tarefaAdicionada = document.getElementById("ListaTarefa");
-  let ListaTarefa = document.createElement("li");
+function adicionarTarefa() {
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    
 
-  let mensagem = document.getElementById("mensagem");
-  let texto ="";
-  if (inserirLista.value.trim() === "") {
-    texto = "Nada digitado!";
-    mensagem.textContent = texto;
-    mensagem.textContent.style.color("#a34743") = mensagem.textContent;
-  } else{
-    ListaTarefa.textContent = tarefa;
-    ListaTarefa.className = "Lista";
-    tarefaAdicionada.appendChild(ListaTarefa);
-    texto = "Tarefa adicionada com sucesso!";
-    mensagem.textContent = texto;
-  }
-  inserirLista.value = "";
+    const mensagem = document.getElementById("mensagem")
+
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+        tarefas.push(tarefa)
+        renderizarTarefas()
+        
+        if(contador===0){
+        const limparListas = document.getElementById("limparListas")
+        let botaoLimparLista = document.createElement("button")
+        botaoLimparLista.textContent =" Limpar tudo"
+        botaoLimparLista.className = "botao_lista"
+        botaoLimparLista.onclick = () => limparLista(limparListas,botaoLimparLista)
+
+
+        limparListas.appendChild(botaoLimparLista) 
+         contador--
+    }
+   
+       
+    }
+
+ 
+
+    inputTarefa.value = ""
+    
+}
+
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+
+
+    
+    for (let i = 0; i < tarefas.length; i++){
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefas[i]
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent ="Remover🚮"
+        botaoRemover.onclick = () => removerTarefa(i)
+        let botaoEditar = document.createElement("button")
+        
+        botaoEditar.className = "editar"
+        botaoEditar.textContent ="Editar✏️"
+        botaoEditar.onclick = () => editarTarefa(i)
+
+        novaTarefa.appendChild(botaoEditar)
+        novaTarefa.appendChild(botaoRemover)
+        listaTarefas.appendChild(novaTarefa)
+
+    }
+}
+
+function removerTarefa(i){
+    tarefas.splice(i,1)
+    renderizarTarefas()
+
+}
+function  editarTarefa(i){
+   let tarefaEditada = prompt("Edite a tarefa:")
+   if(tarefaEditada.trim() !== ""){
+    tarefas[i] = tarefaEditada
+    renderizarTarefas()
+   }
+}
+function limparLista(limparListas,botaoLimparLista){
+    tarefas.length = 0
+    renderizarTarefas()
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent = "Lista de tarefas limpa com sucesso!"
+    
+    limparListas.removeChild(botaoLimparLista)
+    contador ++
+
+    
 }
